d309658d2a658275dc9641f6d13d77a7de28632c
Radica Ristova <127231682+RistovaRadica@users.noreply.github.com>
2023-05-31 21:41:41 +0200

    git comit

--- src/test/java/SILab2Test.java
+++ src/test/java/SILab2Test.java
@@ -1,2 +1,18 @@
-package PACKAGE_NAME;public class SILab2Test {
+import org.junit.jupiter.api.Test;
+
+public class SILab2Test {
+
+    @Test
+    public void testValidEmail() {
+        String email = "example@example.com";
+
+        assert email.matches("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,}") : "Невалиден е-маил.";
+    }
+
+    @Test
+    public void testValidUsername() {
+        String username = "example123";
+
+        assert username.matches("[A-Za-z0-9]+") : "Невалидно корисничко име.";
+    }
 }
